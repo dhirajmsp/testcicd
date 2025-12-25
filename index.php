509ebbbc9c776a5f635b0166fed21e372524e8cf
Dhiraj Patel <dhiraj.p@msp-group.co.uk>
2025-12-25 12:33:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ $errors = [];
 $old = ['name' => '', 'email' => '', 'message' => ''];
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  
     // Trim and sanitize inputs
     $old['name'] = isset($_POST['name']) ? trim($_POST['name']) : '';
     $old['email'] = isset($_POST['email']) ? trim($_POST['email']) : '';
